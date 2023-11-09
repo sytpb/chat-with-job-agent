22b9486b3f30dcc6e0c30ea31f6cd8f289e0b828
sytpb <songyantao@foxmail.com>
2023-11-09 18:41:33 +0200

    support login with Google OAuth

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from '@/auth'
-import { LoginButton } from '@/components/login-button'
+import { GoogleButton } from '@/components/google-button'
+import { LoginButton } from '@/components/github-button'
 import { redirect } from 'next/navigation'
 
 export default async function SignInPage() {
@@ -9,8 +10,9 @@ export default async function SignInPage() {
     redirect('/')
   }
   return (
-    <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
+    <div className="flex h-[calc(100vh-theme(spacing.16))] flex-col items-center justify-center gap-y-2 py-10">
       <LoginButton />
+      <GoogleButton />
     </div>
   )
 }
--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth, { type DefaultSession } from 'next-auth'
 import GitHub from 'next-auth/providers/github'
+import Google from 'next-auth/providers/google'
 
 declare module 'next-auth' {
   interface Session {
@@ -15,13 +16,24 @@ export const {
   auth,
   CSRF_experimental // will be removed in future
 } = NextAuth({
-  providers: [GitHub],
+  providers: [GitHub,Google({
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET
+  })],
   callbacks: {
     jwt({ token, profile }) {
-      if (profile) {
-        token.id = profile.id
-        token.image = profile.avatar_url || profile.picture
+      if (profile?.sub) { // Google profiles have a 'sub' field
+        token.id = String(profile.sub);
+        token.image = profile.picture;
+      } else if (profile?.id) { // GitHub profiles have an 'id' field
+        token.id = profile?.id;
+        token.image = profile.avatar_url;
       }
+      if (profile) {
+
+        console.log({a:profile?.image , b:profile?.picture},profile)
+      }
+      
       return token
     },
     authorized({ auth }) {
